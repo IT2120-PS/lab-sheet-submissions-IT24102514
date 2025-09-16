9cffc1b8af1906b7e6c585aa1528c59d292e5631
IT24102514 <IT24102514@my.sliit.lk>
2025-09-16 11:50:18 +0530

    Add files via upload

---
+++ Lab Sheet 07/PS LAB 07/IT24102514.R
@@ -0,0 +1,11 @@
+#Exercise_1
+P1 <- punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+P1
+#Exercise_2
+P2 <- pexp(2, rate = 1/3)
+P2
+#Exercise_3
+P3_i <- 1 - pnorm(130, mean = 100, sd = 15)
+P3_i
+P3_ii <- qnorm(0.95, mean = 100, sd = 15)
+P3_ii
